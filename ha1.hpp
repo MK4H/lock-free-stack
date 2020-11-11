25654c95da0a75437c7113fcecd7bfa1bbfa0782
Karel Madera <karel.madera@eyen.se>
2020-11-11 22:24:47 +0100

    Fix Issue #4
    Fixes access after free when releasing memory.
    
    There is no straightforward way to release memory while popping, as
    there may be threads holding the reference basically indefinitely before
    accessing it again.
    
    This forces us to deffer the releasing of memory until the whole LFStack
    is destroyed, making it someone else's problem if destructor runs
    concurrently with something from the LFStack.
    
    To achieve this, we create a pool of "free" or empty nodes, making use
    of things we already implemented, like pushing and popping from a linked
    list.
    
    The pool creates a second stack, which is used to store these empty
    nodes, and instead of allocating new node on every push into the
    LFStack, we try to retrieve an already allocated node from the pool
    before allocating new node. This makes everything work correctly, while
    minimizing the amount of new code needed.
    
    The implementations of push and pop were separated into push_to_handle
    and try_pop methods, which implement the push and pop into handle passed
    as an argument. This allows us to use these methods to push and pop both
    from the head_handle and from the pool_handle.
    
    Also implemented a destructor, so that the memory is cleared and
    deallocated after the LFstack is destroyed.

--- ha1.hpp
+++ ha1.hpp
@@ -35,6 +35,10 @@ struct LinkedListNode {
 	T value;
 	LinkedListNode *next;
 
+	LinkedListNode()
+		:value(), next(nullptr)
+	{ }
+
 	LinkedListNode(const T &value)
 		:value(value), next(nullptr)
 	{ }
@@ -45,38 +49,51 @@ struct LinkedListNode {
 template <typename T>
 class LFStack {
 public:
+	LFStack();
+	~LFStack();
+
 	void push(const T &v);
 	T pop();
 
 	bool empty() const;
 private:
 	std::atomic<impl::LinkedListHandle<T>> head_handle;
+	std::atomic<impl::LinkedListHandle<T>> pool_handle;
+
+	impl::LinkedListNode<T>* get_new_node();
+	void push_to_handle(std::atomic<impl::LinkedListHandle<T>> &handle, impl::LinkedListNode<T> *n_head);
+	std::tuple<bool, impl::LinkedListNode<T>*> try_pop(std::atomic<impl::LinkedListHandle<T>> &handle, impl::LinkedListHandle<T> &c_handle);
 };
+
+template<typename T>
+LFStack<T>::LFStack()
+	:head_handle()
+{ }
+
+template<typename T>
+LFStack<T>::~LFStack() {
+	impl::LinkedListNode<T> *head = head_handle.load(std::memory_order_acquire).head;
+	while (head != nullptr) {
+		auto to_delete = head;
+		head = head->next;
+		delete to_delete;
+	}
+
+	head = pool_handle.load(std::memory_order_acquire).head;
+	while (head != nullptr) {
+		auto to_delete = head;
+		head = head->next;
+		delete to_delete;
+	}
+}
 
 template <typename T>
 void LFStack<T>::push(const T &v) {
 
-	impl::LinkedListNode<T> *n_head = new impl::LinkedListNode<T>(v);
+	impl::LinkedListNode<T> *n_head = get_new_node();
+	n_head->value = v;
 
-	impl::LinkedListHandle<T> c_handle = head_handle.load(std::memory_order_acquire);
-	for (;;) {
-		impl::LinkedListHandle<T> n_handle = c_handle;
-		n_head->next = n_handle.head;
-		n_handle.head = n_head;
-		++n_handle.tag;
-		// On success, we need to release all the writes
-		// As per Issue #2, when compare exchange fails,
-		// it loads the actual value of head_handle to c_handle,
-		// we don't have to load it manually
-		if (head_handle.compare_exchange_weak(
-				c_handle,
-				n_handle,
-				std::memory_order_release,
-				std::memory_order_acquire)
-			){
-			break;
-		}
-	}
+	push_to_handle(head_handle, n_head);
 }
 
 template <typename T>
@@ -85,15 +102,64 @@ T LFStack<T>::pop() {
 
 	impl::LinkedListHandle<T> c_handle = head_handle.load(std::memory_order_acquire);
 	for (;;) {
-		impl::LinkedListHandle<T> n_handle = c_handle;
+		auto [success, n_popped] = try_pop(head_handle, c_handle);
 
-		popped = n_handle.head;
-		n_handle.head = n_handle.head->next;
+		if (success) {
+			popped = n_popped;
+			break;
+		}
+	}
+
+	if (popped == nullptr) {
+		throw new std::runtime_error{"Popping from empty stack"};
+	}
+
+	T value = std::move(popped->value);
+	push_to_handle(pool_handle, popped);
+	return value;
+}
+
+template <typename T>
+bool LFStack<T>::empty() const {
+	// Could probably go with relaxed memory order
+	return head_handle.load(std::memory_order_acquire).head == nullptr;
+}
+
+template <typename T>
+impl::LinkedListNode<T>* LFStack<T>::get_new_node() {
+	constexpr int max_retries = 3;
+
+	impl::LinkedListHandle<T> c_handle = pool_handle.load(std::memory_order_acquire);
+
+	impl::LinkedListNode<T> *popped = nullptr;
+	for (int tries = 0; tries < max_retries; ++tries) {
+		auto [success, n_popped] = try_pop(pool_handle, c_handle);
+
+		if (success) {
+			popped = n_popped;
+			break;
+		}
+	}
+
+	if (popped == nullptr) {
+		return new impl::LinkedListNode<T>();
+	}
+	return popped;
+}
+
+/**
+ * handle MUST be a reference, otherwise it would not be updated by compare_exchange
+ */
+template <typename T>
+void LFStack<T>::push_to_handle(std::atomic<impl::LinkedListHandle<T>> &handle, impl::LinkedListNode<T> *n_head) {
+	impl::LinkedListHandle<T> c_handle = handle.load(std::memory_order_acquire);
+	for (;;) {
+		impl::LinkedListHandle<T> n_handle = c_handle;
+		n_head->next = n_handle.head;
+		n_handle.head = n_head;
 		++n_handle.tag;
-		// As per Issue #2, when compare exchange fails,
-		// it loads the actual value of head_handle to c_handle,
-		// we don't have to load it manually
-		if (head_handle.compare_exchange_weak(
+
+		if (handle.compare_exchange_weak(
 				c_handle,
 				n_handle,
 				std::memory_order_release,
@@ -102,16 +168,32 @@ T LFStack<T>::pop() {
 			break;
 		}
 	}
-
-	T value = std::move(popped->value);
-	delete popped;
-	return value;
 }
 
+/**
+ * handle and c_handle MUST be references, otherwise they will not be updated by compare_exchange
+ */
 template <typename T>
-bool LFStack<T>::empty() const {
-	// Could probably go with relaxed memory order
-	return head_handle.load(std::memory_order_acquire).head == nullptr;
+std::tuple<bool, impl::LinkedListNode<T>*> LFStack<T>::try_pop(std::atomic<impl::LinkedListHandle<T>> &handle, impl::LinkedListHandle<T> &c_handle) {
+	impl::LinkedListHandle<T> n_handle = c_handle;
+	impl::LinkedListNode<T> *popped = nullptr;
+	if (n_handle.head == nullptr) {
+		return {true, nullptr};
+	}
+
+	popped = n_handle.head;
+	n_handle.head = n_handle.head->next;
+	++n_handle.tag;
+
+	if (handle.compare_exchange_weak(
+			c_handle,
+			n_handle,
+			std::memory_order_release,
+			std::memory_order_acquire)
+		){
+		return {true, popped};
+	}
+	return {false, nullptr};
 }
 
 #endif // !NPRG058_HA1_LOCKFREE_GUARD__
